1e3669ef1bd9a0cfaa49509d900db1ecdfc6e7ec
Anhelina Sych <angelina.batayeva@gmail.com>
2023-10-31 22:20:17 +0200

    test commit1

---
+++ src/lesson1/Task2.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Saturday,November,2023");
+    }
+}
+
+
---
+++ src/lesson1/Task3.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String name= in.next();
+        System.out.println("Hello "+name );
+    }
+}
